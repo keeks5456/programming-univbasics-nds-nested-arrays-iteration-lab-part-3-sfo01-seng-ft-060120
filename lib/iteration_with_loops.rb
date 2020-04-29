4da4e593f5902f6bc2689566b58ab66100051532
Akiko Green <akikogreen08@gmail.com>
2020-04-29 02:03:33 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,9 +1,7 @@
 def join_nested_strings(src)
 new_string = " "
 row_index = 0 
-
 while row_index < src.count do
-  
   element_index = 0 
   while element_index < src[row_index].count do
     if src[row_index][element_index].class == String
