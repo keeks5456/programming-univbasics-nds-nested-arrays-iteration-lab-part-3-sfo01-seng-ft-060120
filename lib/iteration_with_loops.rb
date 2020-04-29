aa92ff6aa991f4ce0ae0ca4e66cdd59b496b0747
Akiko Green <akikogreen08@gmail.com>
2020-04-29 02:02:05 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,13 +7,13 @@ while row_index < src.count do
   element_index = 0 
   while element_index < src[row_index].count do
     if src[row_index][element_index].class == String
-      new_string << src[row_index][element_index]
+      new_string << src[row_index][element_index].concat("")
     end 
     element_index += 1 
   end
   row_index += 1 
 end
-new_string.concat("")
+new_string
 end
 
 
